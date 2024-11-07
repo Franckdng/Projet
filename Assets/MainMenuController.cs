281b842fb8300618e0e5a20b8a8168510511963b
Franckdng <fduong@et.esiea.fr>
2024-11-07 11:40:33 -0500

    free

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject optionsCanvas; 
+    // Start is called before the first frame update
+    public void Startgame()
+    {
+        SceneManager.LoadScene("NomDeLaScèneDuJeu");
+    }
+
+  
+    public void Quitgame()
+    {
+        Debug.Log("Quitter le jeu");
+        Application.Quit();
+    }
+}
