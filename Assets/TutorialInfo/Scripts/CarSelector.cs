b138cf30d781b29e02c4f082a4d1601f9275d1ae
Franckdng <153991083+Franckdng@users.noreply.github.com>
2024-11-12 18:55:51 -0500

    Voiture

--- Assets/TutorialInfo/Scripts/CarSelector.cs
+++ Assets/TutorialInfo/Scripts/CarSelector.cs
@@ -5,27 +5,39 @@ public class CarSelector : MonoBehaviour
 {
     [SerializeField] private Button previousButton;
     [SerializeField] private Button nextButton;
+    [SerializeField] private GameObject[] carModels; // Tableau de modèles de voitures
     private int currentCar;
 
     private void Awake()
     {
-        SelectCar(0);
-    }
-
-    private void SelectCar(int _index)
-    {
-        previousButton.interactable = (_index != 0);
-        nextButton.interactable = (_index != transform.childCount - 1);
-
-        for (int i = 0; i < transform.childCount; i++)
+        // Vérifie si des modèles de voitures sont assignés dans le tableau
+        if (carModels.Length > 0)
         {
-            transform.GetChild(i).gameObject.SetActive(i == _index);
+            SelectCar(0); // Initialise avec le premier modèle de voiture
+        }
+        else
+        {
+            Debug.LogWarning("Aucun modèle de voiture n'est assigné dans le tableau carModels.");
         }
     }
 
     public void ChangeCar(int _change)
     {
         currentCar += _change;
+        currentCar = Mathf.Clamp(currentCar, 0, carModels.Length - 1); // Limite l'index pour éviter les erreurs
+        Debug.Log("ChangeCar called with change: " + _change + ", new currentCar: " + currentCar);
         SelectCar(currentCar);
+    }
+
+    private void SelectCar(int _index)
+    {
+        Debug.Log("SelectCar called with index: " + _index);
+        previousButton.interactable = (_index != 0);
+        nextButton.interactable = (_index != carModels.Length - 1);
+
+        for (int i = 0; i < carModels.Length; i++)
+        {
+            carModels[i].SetActive(i == _index);
+        }
     }
 }
