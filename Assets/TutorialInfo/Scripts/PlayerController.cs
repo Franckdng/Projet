ee3c600fa110a82172a1485e8fd2155534da5369
Binasta <bastian.beaufils44@gmail.com>
2024-11-26 20:53:13 +0100

    sdf

--- Assets/TutorialInfo/Scripts/PlayerController.cs
+++ Assets/TutorialInfo/Scripts/PlayerController.cs
@@ -11,7 +11,13 @@ public class PlayerController : MonoBehaviour
     public float horizontalinput;
     public float verticalinput;
     public bool ausol;
+<<<<<<< Updated upstream
     public float distancesol = 0.1f;
+=======
+    public bool mur;
+    public float distancesol = 0.2f;
+    public Rigidbody rb;
+>>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
@@ -22,11 +28,74 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         ausol = Physics.Raycast(transform.position + Vector3.up*0.1f, Vector3.down, distancesol);
         
         horizontalinput = Input.GetAxis("Horizontal");
         verticalinput = Input.GetAxis("Vertical");
         
+=======
+        /*   
+           ausol = Physics.Raycast(transform.position + Vector3.up*0.1f, Vector3.down, distancesol);
+
+           horizontalinput = button.b;
+           verticalinput = button.a;
+
+           if (ausol)
+           {
+           if (verticalinput == 1 && speed < 70) { speed += 10 * Time.deltaTime; };
+           if (verticalinput == -1 && speed > -70 && speed <0) { speed -= 10 * Time.deltaTime; };
+           if (verticalinput == -1 && speed > 0) { speed -= 20 * Time.deltaTime; }
+           if (verticalinput < 0.7 && speed > 0) { speed-= 20 * Time.deltaTime; };
+           if (verticalinput > -0.7 && speed < 0) { speed += 20 * Time.deltaTime; };
+           }
+           else
+           {
+               if(speed > 0 ) { speed -= 20 * Time.deltaTime; };
+               if (speed < 0) {speed += 20 * Time.deltaTime;
+           }
+
+           }
+
+           //if (speed < 0.07 && speed > -0.07   ) { speed = 0; };
+           if(verticalinput < -0.4 && speed>0 && ausol) { turnspeed = 100; }
+           else turnspeed = speed;
+           if (turnspeed <30) turnspeed = 30;
+           //if (horizontalinput != -1 && turnspeed > 100) { turnspeed -= 70 * Time.deltaTime; }
+           //if ((horizontalinput == -1 || horizontalinput == 1) && turnspeed < 80) { turnspeed += speed*Time.deltaTime; }
+           //if (horizontalinput != -1 && horizontalinput != 1 && turnspeed > 30) { turnspeed -= speed * Time.deltaTime; }
+           //transform.Translate(Vector3.right * Time.deltaTime * turnspeed * horizontalinput);
+
+
+
+           if (speed > 0.07 || speed < -0.07)
+           {
+               //move forward
+               transform.Translate(Vector3.forward * Time.deltaTime * speed);
+               transform.Rotate(Vector3.up * Time.deltaTime * turnspeed * horizontalinput);
+           }
+           else
+           {
+               transform.Translate(0,0,0);
+           }
+           */
+
+
+        
+        
+        // Vérification si la voiture est au sol
+        ausol = Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, distancesol);
+
+        // Obtenir les entrées
+
+        horizontalinput = button.b;
+        verticalinput = button.a;
+
+        //horizontalinput = Input.GetAxis("Horizontal");
+        //verticalinput = Input.GetAxis("Vertical");
+
+        // Accélération et décélération
+>>>>>>> Stashed changes
         if (ausol)
         {
         if (verticalinput == 1 && speed < 70) { speed += 10 * Time.deltaTime; };
@@ -40,6 +109,7 @@ public class PlayerController : MonoBehaviour
             if(speed > 0 ) { speed -= 20 * Time.deltaTime; };
             if (speed < 0) {speed += 20 * Time.deltaTime;
         }
+       
 
         }
 
@@ -60,6 +130,24 @@ public class PlayerController : MonoBehaviour
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
             transform.Rotate(Vector3.up * Time.deltaTime * turnspeed * horizontalinput);
         }
+<<<<<<< Updated upstream
         
     }
+=======
+        else { transform.Translate(0, 0, 0); }
+        // Rotation uniquement si la voiture bouge
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall") // Si c'est un mur
+        {
+            mur = true;
+            speed = 0;
+            rb.velocity = Vector3.zero; // Stoppe immédiatement la vitesse
+            rb.angularVelocity = Vector3.zero; // Stoppe la rotation
+        }
+        else mur = false;
+    }
+
+>>>>>>> Stashed changes
 }
